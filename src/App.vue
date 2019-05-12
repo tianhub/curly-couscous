b9a024d06fc0b4972b5d4e8e235541a2b3040bbe
tian <1149375181@qq.com>
2019-05-12 15:42:27 +0800

    'sdf';

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <template>
   <div id="app">
-    <div>离开洛杉矶的</div>
+    <div>离开洛杉矶撒地方的</div>
   </div>
 </template>
 
 <script>
 
-export default {
-  name: 'app',
-  components: {
+  export default {
+    name: 'app',
+    components: {}
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
